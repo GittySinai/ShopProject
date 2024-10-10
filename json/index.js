38e5887aaed487f5fce22664cc82b1456564f3c0
Gitty Sinai <gitty.sinai214@gmail.com>
2024-10-10 10:52:02 +0300

    import json

--- json/index.js
+++ json/index.js
@@ -1,1 +1,9 @@
-import data from p
+import data from './products.json' assert{type:"json"}
+console.log(data)
+
+const notebooks = data.notebooks;
+const crafts = data.craft;
+
+// הדפסת התוצאות לקונסול
+console.log('Notebooks:', notebooks);
+console.log('Crafts:', crafts);
